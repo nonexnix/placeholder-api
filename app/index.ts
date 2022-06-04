45fd68bbcdd50b3325855ea02b11b4f1a9efe4d9
ntabucejo <ntabucejo@gmail.com>
2022-06-04 13:23:24 +0800

    Add user and post

--- app/index.ts
+++ app/index.ts
@@ -2,10 +2,56 @@ import express, { Request, Response } from 'express'
 import database from '../library/database'
 
 const app = express()
+const port = process.env.PORT || 8080
 
-app.get('/', async (request, response) => {
-  const users = await database.user.findMany()
-  response.status(200).json(users)
+app.get('/api/users', async (request: Request, response: Response) => {
+  const { method } = request
+  if (method !== 'GET') response.status(400).json({ message: 'error' })
+  try {
+    const users = await database.user.findMany()
+    response.status(200).json(users)
+  } catch (error) {
+    response.status(500).json({ message: error })
+  }
 })
 
-app.listen(3001)
+app.get('/api/users/:id', async (request: Request, response: Response) => {
+  const { method } = request
+  if (method !== 'GET') response.status(400).json({ message: 'error' })
+  try {
+    const { id } = request.params
+    const users = await database.user.findUnique({
+      where: { id },
+    })
+    response.status(200).json(users)
+  } catch (error) {
+    response.status(500).json({ message: error })
+  }
+})
+
+app.get('/api/posts', async (request: Request, response: Response) => {
+  const { method } = request
+  if (method !== 'GET') response.status(400).json({ message: 'error' })
+  try {
+    const posts = await database.post.findMany()
+    response.status(200).json(posts)
+  } catch (error) {
+    response.status(500).json({ message: error })
+  }
+})
+
+app.get('/api/posts/:id', async (request: Request, response: Response) => {
+  const { method } = request
+  if (method !== 'GET') response.status(400).json({ message: 'error' })
+  try {
+    const { id } = request.params
+    const posts = await database.post.findUnique({
+      where: { id },
+    })
+    response.status(200).json(posts)
+  } catch (error) {
+    response.status(500).json({ message: error })
+  }
+})
+
+app.listen(port)
